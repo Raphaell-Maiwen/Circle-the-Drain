950656b002ab0a92b6314e8372a3b69c1b57170b
Raphaëll Maïwen <raphaell.maiwen@gmail.com>
2026-04-08 19:42:21 -0400

    Ok-ish state. Will probably scrap all that

--- Assets/Scripts/TriggerZones/BookTriggerZone.cs
+++ Assets/Scripts/TriggerZones/BookTriggerZone.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BookTriggerZone : InteractableTriggerZone
@@ -11,20 +12,44 @@ public class BookTriggerZone : InteractableTriggerZone
     [SerializeField] private float _openingSpeed;
     [SerializeField] private float _openingRotationSpeed;
     
+    [SerializeField] private float startAnimationTime;
+    [SerializeField] private float endAnimationTime;
+    
     [SerializeField] private BookText _bookText;
+    
+    private AnimatorClipInfo _currentClipInfo;
+    private float _clipLength;
 
     private void Start()
     {
         /*_bookAnimator.speed = 0f;
         _bookAnimator.Play("YOUR_ANIMATION_NAME_HERE",0,0);*/
         
+        //Armature|ArmatureAction
+        
         // Fetch the current Animation clip information for the base layer (layer 0)
-        AnimatorClipInfo[] m_CurrentClipInfo = _bookAnimator.GetCurrentAnimatorClipInfo(0);
+        _currentClipInfo = _bookAnimator.GetCurrentAnimatorClipInfo(0)[0];
+        _clipLength = _currentClipInfo.clip.length;
+        
+        _bookAnimator.Play("Armature|ArmatureAction", 0, startAnimationTime);
 
         // Access the Animation clip name (for the first clip in the list)
-        if (m_CurrentClipInfo.Length > 0)
+    }
+
+    private void Update()
+    {
+        AnimatorStateInfo animState = _bookAnimator.GetCurrentAnimatorStateInfo(0);
+
+        // normalizedTime goes past 1.0 when the clip ends (e.g. 1.3 = second loop at 30%)
+        float normalizedTime = animState.normalizedTime % 1f;
+        float timeInSeconds = normalizedTime * _clipLength;
+
+        Debug.Log("Current time in seconds: " + timeInSeconds);
+
+        // Loop back to start point when we reach the end point
+        if (normalizedTime >= endAnimationTime)
         {
-            Debug.Log("Current Clip Name: " + m_CurrentClipInfo[0].clip.name);
+            _bookAnimator.Play("Armature|ArmatureAction", 0, startAnimationTime);
         }
     }
 
