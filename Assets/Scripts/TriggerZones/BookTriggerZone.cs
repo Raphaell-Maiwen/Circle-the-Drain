f68ea6996ce371d0a87e742d99e153291c70813f
Raphaëll Maïwen <raphaell.maiwen@gmail.com>
2026-05-19 13:38:51 -0400

    Moved change state in a Coroutine

--- Assets/Scripts/TriggerZones/BookTriggerZone.cs
+++ Assets/Scripts/TriggerZones/BookTriggerZone.cs
@@ -8,11 +8,14 @@ public class BookTriggerZone : InteractableTriggerZone
     [SerializeField] private Transform _bookTransform;
     [SerializeField] private Transform _bookOpenAnchor;
     [SerializeField] private Transform _bookClosedAnchor;
-    [SerializeField] private float _openingSpeed;
+    [SerializeField] private float _changingStateSpeed;
     [SerializeField] private GameObject _closedBookGO;
     [SerializeField] private GameObject _openedBookGO;
     
     [SerializeField] private BookText _bookText;
+
+    private float _timeSinceChangeStarted = 0f;
+    private Coroutine _changingStateCoroutine;
 
     protected override void OnPlayerEnter()
     {
@@ -30,10 +33,9 @@ public class BookTriggerZone : InteractableTriggerZone
     {
         CharacterInputHandler.Instance.PlayerInput.actions.FindActionMap("Player").Disable();
         CharacterInputHandler.Instance.PlayerInput.actions.FindActionMap("Cutscene").Enable();
-        //Ici pour ouvrir
 
-        _closedBookGO.SetActive(false);
-        _openedBookGO.SetActive(true);
+        if(_changingStateCoroutine != null) StopCoroutine(_changingStateCoroutine);
+        _changingStateCoroutine = StartCoroutine(ChangeBookState(_bookClosedAnchor, _bookOpenAnchor, true));
         
         CharacterInputHandler.Instance.EnableToggleReadingBook();
         
@@ -44,14 +46,26 @@ public class BookTriggerZone : InteractableTriggerZone
     {
         _interactMessenger.OnInteractPressed?.Invoke(null);
         
-        //Ici pour fermer
-        _closedBookGO.SetActive(true);
-        _openedBookGO.SetActive(false);
+        if(_changingStateCoroutine != null) StopCoroutine(_changingStateCoroutine);
+        _changingStateCoroutine = StartCoroutine(ChangeBookState(_bookOpenAnchor, _bookClosedAnchor, false));
 
         CharacterInputHandler.Instance.PlayerInput.actions.FindActionMap("Player").Enable();
         CharacterInputHandler.Instance.PlayerInput.actions.FindActionMap("Cutscene").Disable();
 
         CharacterInputHandler.Instance.DisableToggleReadingBook();
+    }
+
+    IEnumerator ChangeBookState(Transform startingPos, Transform endingPos, bool isOpening)
+    {
+        //yield return new WaitForSeconds(_changingStateSpeed);
+        yield return null;
+
+        _timeSinceChangeStarted = 0f;
+        
+        _openedBookGO.SetActive(isOpening);
+        _closedBookGO.SetActive(!isOpening);
+
+        _changingStateCoroutine = null;
     }
 }
 
