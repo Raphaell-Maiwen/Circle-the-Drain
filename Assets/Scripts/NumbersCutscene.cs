eb06ea7c30ecbe1c7da10aee39882a0b629112bb
Raphaëll Maïwen <raphaell.maiwen@gmail.com>
2026-07-15 17:57:53 -0400

    Numbers

---
+++ Assets/Scripts/NumbersCutscene.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class NumbersCutscene : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer _videoPlayer;
+    [SerializeField] private MeshRenderer _renderer;
+    [SerializeField] private Collider _collider;
+    [SerializeField] private float _speed;
+
+    [SerializeField] private bool _test;
+
+    #if UNITY_EDITOR
+    private void Start()
+    {
+        if (_test)
+        {
+            StartCoroutine(StartVideo());
+        }
+    }
+#endif
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(StartVideo());
+        }
+    }
+
+     IEnumerator StartVideo()
+     {
+         _renderer.enabled = true;
+        _videoPlayer.enabled = true;
+        _collider.enabled = false;
+
+        yield return new WaitForSeconds(0.1f);
+        yield return null;
+        //Maybe just a yield return null?
+
+        while (_videoPlayer.isPlaying)
+        {
+            transform.position += transform.up * _speed * Time.deltaTime;
+            yield return null;
+        }
+        
+        Destroy(gameObject);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
