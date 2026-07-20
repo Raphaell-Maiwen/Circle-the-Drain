af491b3f5ce263b6cac9515a6028dbd2709c8411
Raphaëll Maïwen <raphaell.maiwen@gmail.com>
2026-07-20 13:56:10 -0400

    Small adjustments in CandyLand

--- Assets/Scripts/CreditsManager.cs
+++ Assets/Scripts/CreditsManager.cs
@@ -1,6 +1,19 @@
+using System;
 using UnityEngine;
 
 public class CreditsManager : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+    [SerializeField] private RectTransform[] _transforms;
     
+    //Faire un scriptable avec toutes les infos des credits
+    //Populate on Start
+
+    private void Update()
+    {
+        foreach (var t in _transforms)
+        {
+            t.anchoredPosition += new Vector2(0, _speed * Time.deltaTime);
+        }
+    }
 }
